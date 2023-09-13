80bd7ba043f419ab1c76d6d588721363688339fd
HoKeiL <hokeileung1106@gmail.com>
2023-09-13 16:08:56 +0100

    created loog in and out function

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { baseURL } from "../utilities/baseURL";
 import { useEffect, useState } from "react";
-import { Select } from "@chakra-ui/react";
+import { Button, Flex, Select, Text } from "@chakra-ui/react";
+import { ResourceCard } from "./ResourceCard";
 
 interface User {
     user_id: number;
@@ -11,6 +12,8 @@ interface User {
 
 export function LandingPage(): JSX.Element {
     const [users, setUsers] = useState<User[]>([]);
+    const [currentUser, setCurrentUser] = useState("");
+    const [isSignIn, setIsSignIn] = useState(false);
     const getAllUsers = async () => {
         try {
             const response = await axios.get(`${baseURL}users`);
@@ -25,13 +28,40 @@ export function LandingPage(): JSX.Element {
         getAllUsers();
     }, []);
 
+    function handleSelectedUser(event: React.ChangeEvent<HTMLSelectElement>) {
+        setCurrentUser(event.target.value);
+    }
+
+    function handleSignInAndOut() {
+        setIsSignIn(!isSignIn);
+        if (isSignIn) {
+            setCurrentUser("");
+        }
+    }
+
     return (
-        <>
-            <Select placeholder="select user">
-                {users.map((user) => {
-                    return <option key={user.user_id}>{user.name}</option>;
-                })}
-            </Select>
-        </>
+        <Flex>
+            {isSignIn ? (
+                <Text>Current user: {currentUser}</Text>
+            ) : (
+                <Select
+                    placeholder="select user"
+                    onChange={(event) => {
+                        handleSelectedUser(event);
+                    }}
+                >
+                    {users.map((user) => {
+                        return <option key={user.user_id}>{user.name}</option>;
+                    })}
+                </Select>
+            )}
+            <Button
+                onClick={handleSignInAndOut}
+                isDisabled={currentUser.length === 0}
+            >
+                {isSignIn ? "Sign out" : "Sign in"}
+            </Button>
+            <ResourceCard />
+        </Flex>
     );
 }
