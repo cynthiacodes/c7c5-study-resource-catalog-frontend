f52253df45e64e61214bf9657e9f38ef896393c5
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-14 12:45:43 +0100

    render each resource as a card

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -8,20 +8,50 @@ import {
     Stack,
     Text,
 } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { baseURL } from "../utilities/baseURL";
 
+interface Resource {
+    resources_id: number;
+    resource_name: string;
+    author_name: string;
+    url: string;
+    description: string;
+    tags: string;
+    content_type: string;
+    recommended_stage: string;
+    date_created: string;
+    user_id: number;
+    creator_opinion: string;
+    creator_reason: string;
+}
 export function ResourceCard(): JSX.Element {
-    return (
-        <Card maxW="sm">
+    const [allResources, setAllResources] = useState<Resource[]>([]);
+    const getAllResources = async () => {
+        try {
+            const response = await axios.get(`${baseURL}resources`);
+            const resourceList = response.data;
+            setAllResources(resourceList);
+            console.log("resource list", resourceList);
+            console.log("all resources state", allResources);
+        } catch (error) {
+            console.error("error", error);
+        }
+    };
+
+    useEffect(() => {
+        getAllResources();
+    }, []);
+
+    const resources = allResources.map((resource) => (
+        <Card maxW="sm" key={resource.resources_id}>
             <CardBody>
                 <Stack mt="6" spacing="3">
-                    <Heading size="md">(Title)</Heading>
-                    <Text>
-                        ( This sofa is perfect for modern tropical spaces,
-                        baroque inspired spaces, earthy toned spaces and for
-                        people who love a chic design with a sprinkle of vintage
-                        design.)
-                    </Text>
-                    <Text color="blue.600">(recommended stage)</Text>
+                    <Heading size="md">{resource.resource_name}</Heading>
+                    <Text>{resource.author_name}</Text>
+                    <Text>{resource.description}</Text>
+                    <Text color="blue.600">{resource.recommended_stage}</Text>
                 </Stack>
             </CardBody>
             <Divider />
@@ -31,5 +61,6 @@ export function ResourceCard(): JSX.Element {
                 </Button>
             </CardFooter>
         </Card>
-    );
+    ));
+    return <>{resources}</>;
 }
