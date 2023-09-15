e262d43f195f8f2986e36803f4f91cf6fd461ab5
Sgnahore <stephaniergnahore@gmail.com>
2023-09-15 15:47:14 +0100

    live search bar function added

---
+++ src/components/Search.tsx
@@ -0,0 +1,11 @@
+// import { Input } from "@chakra-ui/react";
+// import { Resource } from "./Interfaces";
+// import { useState } from "react";
+
+// export function Search(): JSX.Element {
+//     return (
+//         <>
+//             <Input placeholder="Basic usage" value={input} />
+//         </>
+//     );
+// }
