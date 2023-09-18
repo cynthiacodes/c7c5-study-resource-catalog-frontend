dd47f61549bfae54fd33410429712469f73e1575
HoKeiL <hokeileung1106@gmail.com>
2023-09-18 15:16:13 +0100

    added react router dom @6

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { NewResource } from "./components/NewResource";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/newresource",
+        element: <NewResource />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <ChakraProvider>
-            <App />
+            <RouterProvider router={router} />
         </ChakraProvider>
     </React.StrictMode>
 );
