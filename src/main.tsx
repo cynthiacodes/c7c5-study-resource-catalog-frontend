cf081a7297c28ee041d5a1f3460da60f0d1cf7ce
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-19 15:32:24 +0100

    removed path to AddNewResource

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { AddNewResource } from "./components/AddNewResource";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
-    },
-    {
-        path: "/newresource",
-        element: <AddNewResource />,
     },
 ]);
 
