626eb265a141d5d51de49f5701feea8be50daa90
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-20 15:17:03 +0100

    moved reactrouter to App component

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { ChakraProvider } from "@chakra-ui/react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-    },
-]);
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <ChakraProvider>
-            <RouterProvider router={router} />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </ChakraProvider>
     </React.StrictMode>
 );
