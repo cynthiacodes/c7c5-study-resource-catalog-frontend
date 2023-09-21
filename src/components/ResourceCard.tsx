87cc69ff5a1a1694ea23d14432ca1951e2a75394
HoKeiL <hokeileung1106@gmail.com>
2023-09-21 16:42:41 +0100

    tidy up

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -26,33 +26,6 @@ export function ResourceCard({
 }: ResourceCardViewProps): JSX.Element {
     const [singleResource, setSingleResource] = useState<Resource | null>();
 
-    const resourcesSummary = allResources.map((resource) => (
-        <>
-            <Card maxW="sm" key={resource.resources_id}>
-                <CardBody>
-                    <Stack mt="6" spacing="3">
-                        <Heading size="md">{resource.resource_name}</Heading>
-                        <Text>{resource.author_name}</Text>
-                        <Text>{resource.description}</Text>
-                        <Text color="blue.600">
-                            {resource.recommended_stage}
-                        </Text>
-                    </Stack>
-                </CardBody>
-                <Divider />
-                <CardFooter>
-                    <Button
-                        variant="solid"
-                        colorScheme="blue"
-                        onClick={() => handleViewMore(resource)}
-                    >
-                        See more
-                    </Button>
-                </CardFooter>
-            </Card>
-        </>
-    ));
-
     const handleViewMore = (resource: Resource) => {
         setSingleResource(resource);
     };
@@ -63,7 +36,32 @@ export function ResourceCard({
                 spacing={4}
                 templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
             >
-                {resourcesSummary}
+                {allResources.map((resource) => (
+                    <Card maxW="sm" key={resource.resources_id}>
+                        <CardBody>
+                            <Stack mt="6" spacing="3">
+                                <Heading size="md">
+                                    {resource.resource_name}
+                                </Heading>
+                                <Text>{resource.author_name}</Text>
+                                <Text>{resource.description}</Text>
+                                <Text color="blue.600">
+                                    {resource.recommended_stage}
+                                </Text>
+                            </Stack>
+                        </CardBody>
+                        <Divider />
+                        <CardFooter>
+                            <Button
+                                variant="solid"
+                                colorScheme="blue"
+                                onClick={() => handleViewMore(resource)}
+                            >
+                                See more
+                            </Button>
+                        </CardFooter>
+                    </Card>
+                ))}
             </SimpleGrid>
             {singleResource && (
                 <DetailedResourceCard
